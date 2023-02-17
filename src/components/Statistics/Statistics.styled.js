be2dae2e2b54c416fc5ac001d44a212ea56d36b8
Inna Kukla <0969125133@ukr.net>
2023-02-17 13:58:54 +0200

    Update Statistics.styled.js

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components'
 
 export const StatisticsSection = styled.section`
-    font-size: 20px;
+    font-size: 15px;
     margin-right: 15px;
     text-align: center;
-    border: 1px solid ;
-    border-radius: 5px;
-    box-shadow: 2px -1px 8px 0px blue;
+    border-radius: 7px;
+    box-shadow: 1px 1px 8px 0px lightslategrey;
+    width: 500px;
 `
 
 
 export const StatisticsTitle = styled.h2`
-    padding: 30px 15px;
+    padding: 40px 15px;
     color: lightslategray;
     font-weight: 500;
     background-color: white;
+    font-size: 25px;
+    text-transform: uppercase;
 `
 export const StatList = styled.ul`
     display: grid;
@@ -23,10 +25,14 @@ export const StatList = styled.ul`
 li {
     display: inline-grid;
     padding: 20px 10px;
-    border: 1px solid;
-    width: 50px;
+    width: 60px;
     background-color: blueviolet;
     color: white;
-    box-shadow: 2px -1px 8px 0px blue;
+    box-shadow: -2px 1px 5px 0px lightslategrey;
 }
+`
+
+export const StatPercentage = styled.span`
+    padding-top: 3px;
+    font-size: 25px;
 `
