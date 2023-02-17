7d3bfdc25bec58972d7f535af8e77ced06137c2a
Inna Kukla <0969125133@ukr.net>
2023-02-17 04:42:18 +0200

    Create TransactionHistory.styled.js

---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const TransactionList = styled.table`
+    font-size: 15px;
+    margin-left: 15px;
+    width: 450px;
+    padding: 10px 0;
+    text-align: center;
+    border: 1px solid #afceff59;
+    border-collapse: collapse;
+    box-shadow: 0px 0px 2px 1px #afceff59;
+
+
+    thead tr{
+        background-color: #00b0ff;
+        color: white;
+        font-size: 20px;
+       
+    }
+    thead tr th {
+        padding-top: 5px;
+        padding-bottom: 5px;
+        font-family: rock salt;
+    }
+    
+    
+    tbody tr:nth-child(odd){
+        background-color: #afceff59;
+    }
+
+    tbody tr:nth-child(even){
+        background-color: #FFFFF0;
+    }
+    tbody tr td{
+        padding-top: 5px;
+        padding-bottom: 5px;
+        font-family: rock salt;
+    }
+`
