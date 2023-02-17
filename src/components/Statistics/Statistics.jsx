e252dda439c2fa6eb01c644728a22dffa5c0ed96
Inna Kukla <0969125133@ukr.net>
2023-02-17 13:58:50 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,21 +1,37 @@
 import PropTypes from 'prop-types'
-import { StatisticsSection, StatisticsTitle, StatList } from "./Statistics.styled"
+import { StatisticsSection, StatisticsTitle, StatList, StatPercentage } from "./Statistics.styled"
 
 export const Statistics =({title , stats}) => {
-    return (
-        <StatisticsSection>
-            <StatisticsTitle>{title}</StatisticsTitle>
 
-            <StatList>
-                {stats.map(item => 
-                <li className="item" key={item.id}>
-                    <span className="label">{item.label}</span>
-                    <span className="percentage">{item.percentage}</span>
-                </li>)}
-            </StatList>
-        </StatisticsSection>
-    )
-}
+    if (!title === '') {
+        return (
+            <StatisticsSection>
+                <StatisticsTitle>{title}</StatisticsTitle>
+    
+                <StatList>
+                    {stats.map(item => 
+                    <li className="item" key={item.id} style={{backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`}}>
+                        <span className="label">{item.label}</span>
+                        <StatPercentage>{item.percentage}</StatPercentage>
+                    </li>)}
+                </StatList>
+            </StatisticsSection>
+        )
+    } else {
+        return (
+            <StatisticsSection>
+                <StatList>
+                    {stats.map(item => 
+                    <li className="item" key={item.id} style={{backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`}}>
+                        <span className="label">{item.label}</span>
+                        <StatPercentage>{item.percentage}</StatPercentage>
+                    </li>)}
+                </StatList>
+            </StatisticsSection>
+        )
+    }
+    }
+    
 
 Statistics.propTypes = {
     avatar: PropTypes.string,
