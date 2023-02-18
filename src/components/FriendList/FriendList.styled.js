e859930b64d00cf5980c91851c0707f1fdc69728
Inna Kukla <0969125133@ukr.net>
2023-02-18 15:54:16 +0200

    Update FriendList.styled.js

--- src/components/FriendList/FriendList.styled.js
+++ src/components/FriendList/FriendList.styled.js
@@ -1,42 +1,37 @@
-import styled from 'styled-components'
-
+import styled from 'styled-components';
 
 export const FriendItem = styled.li`
-    display: flex;
-    border: 1px solid lightgray;
-    border-radius: 3px;
+  display: flex;
+  border: 1px solid lightgray;
+  border-radius: 3px;
 
-    align-items: center;
-    padding: 15px;
-    width: 250px;
-    justify-content: flex-start;
-    box-shadow: 3px 3px 3px 4px lightgray;
+  align-items: center;
+  padding: 15px;
+  width: 250px;
+  justify-content: flex-start;
+  box-shadow: 3px 3px 3px 4px lightgray;
 
-    :not(:last-child){
-        margin-bottom: 15px;
-    }
+  :not(:last-child) {
+    margin-bottom: 15px;
+  }
 
-    svg{
-        margin-left: 10px;
-        margin-right: 10px;
-        position: relative;
-        top: -7px;
+  svg {
+    margin-left: 10px;
+    margin-right: 10px;
+    position: relative;
+    top: -7px;
 
-        fill: ${props => {
-        if (props.status === true) {
-            return 'green'
-        } else {
-            return 'red'
-        }
-        
+    fill: ${props => {
+      if (props.status === true) {
+        return 'green';
+      } else {
+        return 'red';
+      }
     }};
-    }
-
-`
-
-
+  }
+`;
 
 export const FriendName = styled.p`
-    font-size: 30px;
-    margin-left: 10px;
-`
+  font-size: 30px;
+  margin-left: 10px;
+`;
