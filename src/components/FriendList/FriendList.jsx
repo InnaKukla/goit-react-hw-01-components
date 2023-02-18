44e72e0ebf70ad28b29fe5464d07ebbd5963c6f0
Inna Kukla <0969125133@ukr.net>
2023-02-18 15:53:30 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,19 +1,26 @@
-import PropTypes from 'prop-types'
-import { FriendListItem } from "./FriendListItem"
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
 
-export const FriendList = ({friends}) => {
-    return (
+export const FriendList = ({ friends }) => {
+  return (
     <ul className="friend-list">
-    {friends.map(friend => <FriendListItem key={friend.id} isOnline={friend.isOnline} avatar={friend.avatar} name={friend.name}/>)}
-    </ul>);
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          isOnline={friend.isOnline}
+          avatar={friend.avatar}
+          name={friend.name}
+        />
+      ))}
+    </ul>
+  );
 };
 
-
 FriendList.propTypes = {
-    friend: PropTypes.exact({
-        id: PropTypes.number.isRequired,
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.bool.isRequired
-    })
-}
+  friend: PropTypes.exact({
+    id: PropTypes.number.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }),
+};
