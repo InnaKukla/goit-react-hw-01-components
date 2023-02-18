2cf28b2233f06de2f2e8f9f4e7c51f5c7b99cd2b
Inna Kukla <0969125133@ukr.net>
2023-02-18 15:54:20 +0200

    Update FriendListItem.jsx

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,23 +1,30 @@
-import PropTypes from 'prop-types'
-import { FriendItem, FriendName } from "./FriendList.styled"
+import PropTypes from 'prop-types';
+import { FriendItem, FriendName } from './FriendList.styled';
 
-
-export const FriendListItem = ({isOnline, avatar, name}) => {
-    return (
+export const FriendListItem = ({ isOnline, avatar, name }) => {
+  return (
     <FriendItem status={isOnline}>
-    <span className="status">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-circle-fill" viewBox="0 0 16 16">
-            <circle cx="8" cy="8" r="8"/>
-        </svg>{isOnline}</span>
-    <img className="avatar" src={avatar} alt="User avatar" width="48" />
-    <FriendName>{name}</FriendName>
-  </FriendItem>);
+      <span className="status">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <circle cx="8" cy="8" r="8" />
+        </svg>
+        {isOnline}
+      </span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <FriendName>{name}</FriendName>
+    </FriendItem>
+  );
 };
 
 FriendListItem.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired
-}
-
-
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
