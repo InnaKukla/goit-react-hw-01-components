4f5186c303a38de4c2a64347f72a87a1872d2d01
Inna Kukla <0969125133@ukr.net>
2023-02-18 15:54:35 +0200

    Update Profile.styled.js

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,69 +1,65 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const ProfileCard = styled.div`
-    width: 300px;
-    margin-right: 15px;
-    margin-left: 15px;
-    text-align: center;
-    border: 1px solid #F8F8FF;
-    border-radius: 7px;
-    box-shadow: 2px -1px 8px 0px  #F8F8FF;
-`
+  width: 300px;
+  margin-right: 15px;
+  margin-left: 15px;
+  text-align: center;
+  border: 1px solid #f8f8ff;
+  border-radius: 7px;
+  box-shadow: 2px -1px 8px 0px #f8f8ff;
+`;
 
 export const ProfileDescription = styled.div`
-   padding: 15px;
-`
+  padding: 15px;
+`;
 export const ProfileAvatar = styled.img`
-    width: 120px;
-    border-radius: 100px;
-    margin-top: 10px;
-`
+  width: 120px;
+  border-radius: 100px;
+  margin-top: 10px;
+`;
 
 export const ProfileName = styled.p`
-   margin-top: 20px;
-   margin-bottom: 20px;
-`
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
 export const ProfileTag = styled.p`
-    font-family: 'comic sans ms';
-    color: darkgray;
-    font-size: 27px;
-`
+  font-family: 'comic sans ms';
+  color: darkgray;
+  font-size: 27px;
+`;
 
 export const ProfileLocation = styled.p`
-    /* font-family: 'comic sans ms'; */
-    color: darkgray;
-    font-size: 28px;
-    margin-bottom: 30px;
-    margin-top: 5px;
-`
+  /* font-family: 'comic sans ms'; */
+  color: darkgray;
+  font-size: 28px;
+  margin-bottom: 30px;
+  margin-top: 5px;
+`;
 
 export const StatsList = styled.ul`
-   display: grid;
-   grid-template-columns: 1fr 1fr 1fr;
-   justify-items: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
 
-   li {
+  li {
     width: 60px;
     display: inline-grid;
     padding: 30px 20px;
     justify-content: center;
-    border-top: 1px solid  #F8F8FF;
-    background-color: #F8F8FF;
+    border-top: 1px solid #f8f8ff;
+    background-color: #f8f8ff;
 
-    :not(:last-child){
-        border-right: 1px solid #F5F5F5;
+    :not(:last-child) {
+      border-right: 1px solid #f5f5f5;
     }
-   }
-
-`
+  }
+`;
 export const StatsListLabel = styled.span`
   font-size: 20px;
   color: darkgray;
-   
-`
+`;
 
 export const StatsQuantity = styled.span`
   font-size: 25px;
-  
-   
-`
+`;
