4a6b1ff90f9fd5c0a6480d34b2a1cded379b2a41
Inna Kukla <0969125133@ukr.net>
2023-02-18 15:54:31 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,43 +1,40 @@
-import PropTypes from 'prop-types'
-import { StatisticsSection, StatisticsTitle, StatList, StatPercentage } from "./Statistics.styled"
+import PropTypes from 'prop-types';
+import {
+  StatisticsSection,
+  StatisticsTitle,
+  StatList,
+  StatPercentage,
+} from './Statistics.styled';
 
-export const Statistics =({title , stats}) => {
-
-    if (!title === '') {
-        return (
-            <StatisticsSection>
-                <StatisticsTitle>{title}</StatisticsTitle>
-    
-                <StatList>
-                    {stats.map(item => 
-                    <li className="item" key={item.id} style={{backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`}}>
-                        <span className="label">{item.label}</span>
-                        <StatPercentage>{item.percentage}</StatPercentage>
-                    </li>)}
-                </StatList>
-            </StatisticsSection>
-        )
-    } else {
-        return (
-            <StatisticsSection>
-                <StatList>
-                    {stats.map(item => 
-                    <li className="item" key={item.id} style={{backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`}}>
-                        <span className="label">{item.label}</span>
-                        <StatPercentage>{item.percentage}</StatPercentage>
-                    </li>)}
-                </StatList>
-            </StatisticsSection>
-        )
-    }
-    }
-    
+export const Statistics = ({ title, stats }) => {
+  return (
+    <StatisticsSection>
+      {title && <StatisticsTitle>{title}</StatisticsTitle>}
+      <StatList>
+        {stats.map(item => (
+          <li
+            className="item"
+            key={item.id}
+            style={{
+              backgroundColor: `#${Math.floor(
+                Math.random() * 16777215
+              ).toString(16)}`,
+            }}
+          >
+            <span className="label">{item.label}</span>
+            <StatPercentage>{item.percentage}</StatPercentage>
+          </li>
+        ))}
+      </StatList>
+    </StatisticsSection>
+  );
+};
 
 Statistics.propTypes = {
-    avatar: PropTypes.string,
-    item: PropTypes.exact({
-        id: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        percentage: PropTypes.number.isRequired
-    })
-}
+  avatar: PropTypes.string,
+  item: PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+};
