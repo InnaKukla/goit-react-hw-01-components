00894e178e73ba1c90c28a36711fbcdfe37fcb7f
Inna Kukla <0969125133@ukr.net>
2023-02-18 15:54:47 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,36 +1,35 @@
-import PropTypes from 'prop-types'
-import { TransactionList } from "./TransactionHistory.styled";
+import PropTypes from 'prop-types';
+import { TransactionList } from './TransactionHistory.styled';
 
-
-export const TransactionHistory =({items}) => {
-return (
+export const TransactionHistory = ({ items }) => {
+  return (
     <TransactionList>
-        <thead>
-            <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-            </tr>
-        </thead>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-        <tbody>
-            {items.map(item => 
-            <tr key={item.id}>
-                <td>{item.type}</td>
-                <td>{item.amount}</td>
-                <td>{item.currency}</td>
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
           </tr>
-            )}
-        </tbody>
+        ))}
+      </tbody>
     </TransactionList>
-    );
+  );
 };
 
 TransactionHistory.propTypes = {
-    item: PropTypes.exact({
-        id: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        amount: PropTypes.string.isRequired,
-        currency: PropTypes.string.isRequired
-    })
-}
+  item: PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }),
+};
